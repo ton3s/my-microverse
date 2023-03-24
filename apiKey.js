d08994c876ca1bc2005fe77109c52d6ff9fb3047
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-03-24 01:33:38 -0500

    Update key

---
+++ apiKey.js
@@ -0,0 +1,4 @@
+/* Copy this into a file named apiKey.js */
+const apiKey = '1MpmeoC9msBMELqMjb0t0cxLftx5mfg2Lhu79FI6F'
+const appId = 'com.gmail.antonio.cunanan.microverse'
+export default { apiKey, appId }
